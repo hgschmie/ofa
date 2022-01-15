a40e32a43be8b285a4d14479fc317cc69db9da5c
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2022-01-15 10:20:17 -0800

    fix up release script

--- do_release.sh
+++ do_release.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <release tag>" 1>&2
+    exit 1
+fi
+
+VERSION=$1 ; shift
+
+git tag ${VERSION}
+changie batch ${VERSION}
+
 RELEASE_NOTES=changes/$(changie latest).md
 
 changie merge
